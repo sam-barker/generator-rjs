cb7de149feb583defb38e785dd8da5c8797a8f35
Samuel Barker <spbarker1994@gmail.com>
2019-04-07 11:19:00 +0100

    Added copying files

--- generators/app/index.js
+++ generators/app/index.js
@@ -12,6 +12,47 @@ module.exports = class extends Generator {
     ]);
   }
 
+  writing () {
+    this.fs.copyTpl(
+      this.templatePath('package.json'),
+      this.destinationPath('package.json'),
+      { name: this.answers.name }
+    );
+    this.fs.copyTpl(
+      this.templatePath('src/index.html'),
+      this.destinationPath('src/index.html'),
+      { name: this.answers.name }
+    );
+    this.fs.copy(
+      this.templatePath('__tests__/components/app/index.spec.js'),
+      this.destinationPath('__tests__/components/app/index.spec.js')
+    );
+    this.fs.copy(
+      this.templatePath('src/index.js'),
+      this.destinationPath('src/index.js')
+    );
+    this.fs.copy(
+      this.templatePath('src/components/app/index.js'),
+      this.destinationPath('src/components/app/index.js')
+    );
+    this.fs.copy(
+      this.templatePath('.babelrc'),
+      this.destinationPath('.babelrc')
+    );
+    this.fs.copy(
+      this.templatePath('.gitignore'),
+      this.destinationPath('.gitignore')
+    );
+    this.fs.copy(
+      this.templatePath('testSetup.js'),
+      this.destinationPath('testSetup.js')
+    );
+    this.fs.copy(
+      this.templatePath('webpack.config.js'),
+      this.destinationPath('webpack.config.js')
+    );
+  }
+
   install () {
     this.npmInstall(
       [
